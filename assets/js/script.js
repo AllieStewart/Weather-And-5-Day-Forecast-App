4327637f64ac3c963c6b947dc70e775974a11245
AllieStewart <megaxis1217@gmail.com>
2023-10-09 18:20:14 -0500

    added starter vars and functions to script.js, added classes to index.html city/date/icon

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+// Define variables here
+var api_key = "";
+var rightNow = dayjs().format('MMM DD, YYYY');
+var searchButton = $('search-button');
+
+// Check if there is already local storage
+$(window).on("load", function() {
+    
+});
+
+// Search button
+searchButton.on("click", function(event)
+{
+    event.preventDefault();
+
+});
+
+// Store recent searches
+function recentSearchList()
+{
+
+}
+
+// Get current weather/location
+function currentWeather ()
+{
+
+}
+
+// Get 5-day forecast
+function fiveDayForecast()
+{
+
+}
+
+// Get data saved locally
+function getLocalStorage()
+{
+
+}
+
+// Save data locally
+function setLocalStorage()
+{
+
+}
