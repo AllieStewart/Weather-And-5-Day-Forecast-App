38d7a9a84b399d43cb59b3f8d5497e37d832b8ba
AllieStewart <megaxis1217@gmail.com>
2023-10-10 15:58:29 -0500

    added id values to index.html, fixed local storage for search bar in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,25 @@
 // Define variables here
 var api_key = "21dd14b3b3fccee814257b2c4c22649e";
 var rightNow = dayjs().format('MMM DD, YYYY');
-var searchButton = $('search-button');
-var city = "";
+var searchButton = $('.search-button');
+var cityNew = document.querySelector("#city-input");
+var input = "";
 
 // Check if there is already local storage
 $(window).on("load", function() {
     
 });
 
-// Search button
+// Search button (get rid of console logs later)
 searchButton.on("click", function(event)
 {
     event.preventDefault();
+    console.log("Message sent!");
 
-    city = $('form-control').val();
+    var city = cityNew.value;
+    localStorage.setItem('cities', city);
+
+    console.log("City: " + city);
 
     // save to local storage, run currentWeather function?
 
@@ -29,16 +34,16 @@ function recentSearchList()
 // Get current weather/location; needs to retrieve API
 function currentWeather (city)
 {
-    var cityURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key;
-    $.ajax(
-        {
-            url: cityURL,
-            method: "GET",
-        }
-    ).then(function (response)
-    {
+    // var cityURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key;
+    // $.ajax(
+    //     {
+    //         url: cityURL,
+    //         method: "GET",
+    //     }
+    // ).then(function (response)
+    // {
 
-    });
+    // });
 
 }
 
