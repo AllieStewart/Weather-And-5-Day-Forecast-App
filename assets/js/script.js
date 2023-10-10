911edec3c6a68e500a39a6edee5513cffc4cb3ba
AllieStewart <megaxis1217@gmail.com>
2023-10-10 17:38:23 -0500

    making localStorage data be used to create a search history; added id for city-date-icon in .html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ var api_key = "21dd14b3b3fccee814257b2c4c22649e";
 var rightNow = dayjs().format('MMM DD, YYYY');
 var searchButton = $('.search-button');
 var cityNew = document.querySelector("#city-input");
-var input = "";
+var city = "";
+//var input = "";
 
 // Check if there is already local storage
 $(window).on("load", function() {
@@ -17,16 +18,17 @@ searchButton.on("click", function(event)
     console.log("Message sent!");
 
     var city = cityNew.value;
-    localStorage.setItem('cities', city);
 
     console.log("City: " + city);
 
-    // save to local storage, run currentWeather function?
+    setLocalStorage(city);
+
+    currentWeather(city);
 
 });
 
 // Store recent searches
-function recentSearchList()
+function recentSearchList(city)
 {
 
 }
@@ -34,16 +36,33 @@ function recentSearchList()
 // Get current weather/location; needs to retrieve API
 function currentWeather (city)
 {
-    // var cityURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key;
-    // $.ajax(
-    //     {
-    //         url: cityURL,
-    //         method: "GET",
-    //     }
-    // ).then(function (response)
-    // {
+    var cityURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key;
+    $.ajax(
+        {
+            url: cityURL,
+            method: "GET",
+            // error: (
+            //     alert("City not found, please check your spelling.");
+            //     return;
+            // )
+        }
+    ).then(function (response)
+    {
+        console.log(response);
 
-    // });
+        $("#city-date-icon").empty();
+
+        // var searchedCity = ;
+        // var icon = ;
+        // var temp = ;
+        // var wind = ;
+        // var humidity = ;
+
+        //append searchedCity.date.icon
+        //append temp, wind, humidity underneath
+
+
+    });
 
 }
 
@@ -56,11 +75,19 @@ function fiveDayForecast()
 // Get data saved locally
 function getLocalStorage()
 {
+    var saved = localStorage.getItem('cities');
+    var saveArr = [];
 
+    saved.trim();
+    saveArr = saved.split(',');
+    for (var i = 0; i < saveArr.length; i++)
+    {
+        recentSearchList(saveArr[i]);
+    }
 }
 
 // Save data locally
-function setLocalStorage()
+function setLocalStorage(city)
 {
-
+    localStorage.setItem('cities', city);
 }
