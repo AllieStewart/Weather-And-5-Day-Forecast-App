5964144d131f9200ee2e5733dd24271ac91aef02
AllieStewart <megaxis1217@gmail.com>
2023-10-11 00:44:24 -0500

    5-day forecast is functional, API works for both current and 5-day; commented out original card designs in index.html, may remove. Still need to fix search history in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,17 +34,21 @@ function recentSearchList(city)
     var buttonNew = $("<button>");
 
     buttonNew.attr('id', 'listBtn');
-    buttonNew.addClass("button is-small search-history-btn");
+    buttonNew.addClass("button btn-outline-success btn-block");
     buttonNew.text(city);
 
     liNew.append(buttonNew);
     $("#search-history").prepend(liNew);
 
-    $("#listBtn").on("click", function ()
+    $("#listBtn").on("click", function(event)
     {
+        event.preventDefault();
+
         var retrieveCity = $(this).text();
+        setLocalStorage(retrieveCity);
         currentWeather(retrieveCity);
     })
+    // only stores one search at a time, hmmm...
 }
 
 // Get current weather/location; needs to retrieve API
@@ -66,6 +70,7 @@ function currentWeather(city)
         $(".city-date-icon").empty();
 
         var searchedCity = $(".city").append(response.name + " ");
+        // date repeats when searching new city; fix append, maybe put to city append instead^
         var date = $(".date").append("(" + rightNow + ")");
         var icon = $('<img class="imgsize">').attr('src', 'https://openweathermap.org/img/wn/' + response.weather[0].icon + '.png');
         var temp = $("<li>").text('Temp: ' + response.main.temp + ' °F');
@@ -76,6 +81,7 @@ function currentWeather(city)
 
         fiveDayForecast(cityID);
 
+        //fix append(date)
         searchedCity.append(date).append(icon).append(temp).append(wind).append(humidity);
 
         $("#city-date-icon").empty();
@@ -87,7 +93,7 @@ function currentWeather(city)
 // Get 5-day forecast
 function fiveDayForecast(city)
 {
-    var cityURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + api_key;
+    var cityURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + city + "&units=imperial&appid=" + api_key;
     $.ajax(
         {
             url: cityURL,
@@ -98,11 +104,19 @@ function fiveDayForecast(city)
         var weatherList = response.list;
         for (var i = 0; i < weatherList.length; i++)
         {
-        // var date = ;
-        // var icon = ;
-        // var temp = ;
-        // var wind = ;
-        // var humidity = ;
+            if (weatherList[i].dt_txt.split(' ')[1] === '12:00:00')
+            {
+                var cityMain = $('<div>');
+                cityMain.addClass('col forecast bg-secondary border border-dark text-white ml-3 mb-3 rounded>');
+                var date = $("<h5>").text(response.list[i].dt_txt.split(" ")[0]);
+                var icon = $('<img>').attr('src', 'http://openweathermap.org/img/w/' + weatherList[i].weather[0].icon + '.png');
+                var temp = $('<p>').text('Temp: ' + weatherList[i].main.temp + ' °F');  
+                var wind = $('<p>').text('Wind: ' + weatherList[i].wind.speed + 'MPH');              
+                var humidity = $('<p>').text('Humidity: ' + weatherList[i].main.humidity + '%');  
+
+                cityMain.append(date).append(icon).append(temp).append(wind).append(humidity);
+                $('#forecast').append(cityMain);
+            }
         }
     })
 }
