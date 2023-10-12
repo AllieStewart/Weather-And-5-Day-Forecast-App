3d7bacb5ddce37ca4a1732dc964ff16ae626eefe
AllieStewart <megaxis1217@gmail.com>
2023-10-11 21:38:20 -0500

    added comments to all files, updated README with more info and more screens, added screens to /assets/images folder

--- README.md
+++ README.md
@@ -1,35 +1,40 @@
 # Challenge-6
 
 ## Description
-Challenge 6 coursework for UT bootcamp. This challenge is about creating
-a webpage that connects to the https://openweathermap.org/api when the name
-of a city is input into the search query. It gives the current weather in
-the city searched, as well as the temperature, wind, and humidity.
-It also gives a 5-day forecast of upcoming weather in the city, using the
-same API, with the same attributes.
+Challenge 6 coursework for UT bootcamp. <br />
+This challenge is about creating a webpage that connects to the <br />
+https://openweathermap.org/api when the name of a city is input into the search query. <br />
+It gives the current weather in the city searched, as well as the temperature, wind, and humidity. <br />
+It also gives a 5-day forecast of upcoming weather in the city, using the <br />
+same API, with the same attributes. <br />
 
 ## Screenshots
----Main page---
+---Main page--- <br />
+![Alt text](assets/images/1main-page.png) <br />
 
----Searching a city---
+---Searching a city--- <br />
+![Alt text](assets/images/2searching-city.png) <br />
 
----Getting current weather for the city---
+---Seeing in local storage, the city info stored--- <br />
+![Alt text](assets/images/3stored-locally.png) <br />
 
----Getting the 5-day forecast for the city---
+---Unfortunately, appends more cities for current weather + 5-day forecast when another search is done--- <br />
+![Alt text](assets/images/4unfortunately.png) <br />
 
----Seeing in local storage, the city info stored---
-
----Seeing search results stored on the left and in local storage---
-
+---Clearing history will clear out local storage + page info--- <br />
+![Alt text](assets/images/5clears-history-and-local-storage.png) <br />
 
 ## Usage
-Simply click on the link and watch the magic happen. Link to the website: https://alliestewart.github.io/challenge-6/
+Simply click on the link and watch the magic happen. <br />
+Link to the website: https://alliestewart.github.io/challenge-6/ 
 
 ## What I Have Learned
-
+I have learned a lot about local storage and how to utilize it to append a search history, as well <br />
+as how APIs work in allowing the webpage to access data it otherwise would never be able to retrieve. <br />
 
 ## Credits
-Made by Allie Stewart. Helpful information provided by https://learn.jquery.com/.
+Made by Allie Stewart. <br />
+Helpful information provided by https://learn.jquery.com/.
 
 ## Licenses
 Please refer to the LICENSE in the repository.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
+//----------------------------------Start of JS file----------------------------------
 // Check if there is already local storage
 $(window).on("load", function() {
     getLocalStorage();
 });
 
-// Define variables here
+// Define variables here;
+// API key, current date, search and clear buttons,
+// the user input value for city name, and an array of city.
 var api_key = "21dd14b3b3fccee814257b2c4c22649e";
 var rightNow = dayjs().format('MM/DD/YYYY');
 var searchButton = $(".search-button");
@@ -11,7 +14,8 @@ var clearButton = $(".clear-button");
 var cityNew = document.querySelector("#city-input");
 var city = [];
 
-// Search button
+// When search button clicked, new city search saved to browser and appended to search history,
+// as well as displaying current weather and 5-day forecast, and setting to local storage.
 searchButton.on("click", function(event)
 {
     event.preventDefault();
@@ -20,26 +24,28 @@ searchButton.on("click", function(event)
     city = cityNew.value;
 
     console.log("City: " + city);
-
+     // Gets current weather + 5-day forecast, sets local storage of city input, and appends it to search history.
     currentWeather(city);
     setLocalStorage(city);
     recentSearchList(city);
 
 });
 
-// Store recent searches
+// Store recent searches.
 function recentSearchList(city)
 {
+    // Creating list items with clickable buttons.
     var liNew = $("<li>");
     var buttonNew = $("<button>");
-
+    // Adding 'listbtn' ID and 'button...' class with city name attached.
     buttonNew.attr('id', 'listBtn');
     buttonNew.addClass("button btn-outline-success btn-block");
     buttonNew.text(city);
-
+    // Appends button to created <li>. Prepends <li> to 'search-history' ID.
     liNew.append(buttonNew);
     $("#search-history").prepend(liNew);
 
+// When the city button is clicked (from 'search history'), retrieves weather info about that city.
     $("#listBtn").on("click", function(event)
     {
         event.preventDefault();
@@ -50,9 +56,10 @@ function recentSearchList(city)
     })
 }
 
-// Get current weather/location; needs to retrieve API
+// Get current weather/location; needs to retrieve API.
 function currentWeather(city)
 {
+    // The current weather API URL with teh ajax method of getting it.
     var cityURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + api_key;
     $.ajax(
         {
@@ -67,29 +74,29 @@ function currentWeather(city)
     {
         console.log(response);
 
-        var searchedCity = $(".city").append($("<h3>" + response.name + " (" + rightNow + ")" + "</h3>"));
-        // date repeats when searching new city; fix append, maybe put to city append instead^
-        //var date = $(".date").append("(" + rightNow + ")");
+        // Appends selectors to the HTML, in the large white box for the current weather today.
+        var searchedCity = $(".city").append($("<p><h3>" + response.name + " (" + rightNow + ")" + "</h3></p>"));
         var icon = $('<img class="imgsize">').attr('src', 'https://openweathermap.org/img/wn/' + response.weather[0].icon + '.png');
         var temp = $("<p>").text('Temp: ' + response.main.temp + ' °F');
         var wind = $("<p>").text('Wind: ' + response.wind.speed + ' MPH');
         var humidity = $("<p>").text('Humidity: ' + response.main.humidity + ' %');
 
+        // Gives city ID to other function for API to use.
         var cityID = response.id;
-
         fiveDayForecast(cityID);
 
+        // Appends values directly to HTML, under 'city-date-icon' ID.
         searchedCity.append(icon).append(temp).append(wind).append(humidity);
-
         $("#city-date-icon").empty();
         $("#city-date-icon").append(searchedCity);
     });
 
 }
 
-// Get 5-day forecast
+// Gets 5-day forecast, needs to retrieve API.
 function fiveDayForecast(city)
 {
+    // The 5-day forecast API URL with ajax method of getting it.
     var cityURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + city + "&units=imperial&appid=" + api_key;
     $.ajax(
         {
@@ -98,11 +105,13 @@ function fiveDayForecast(city)
         }
     ).then(function(response)
     {
+        // Getting list from the URL response, looping into five sets of data.
         var weatherList = response.list;
         for (var i = 0; i < weatherList.length; i++)
         {
             if (weatherList[i].dt_txt.split(' ')[1] === '12:00:00')
             {
+                // Bulk of it, adds selectors to the HTML, makes 5 cards for the 5-day forecast
                 var cityMain = $('<div>');
                 cityMain.addClass('col forecast bg-secondary border border-dark text-white ml-3 mb-3 rounded>');
                 var date = $("<h5>").text(response.list[i].dt_txt.split(" ")[0]);
@@ -110,7 +119,7 @@ function fiveDayForecast(city)
                 var temp = $('<p>').text('Temp: ' + weatherList[i].main.temp + ' °F');  
                 var wind = $('<p>').text('Wind: ' + weatherList[i].wind.speed + ' MPH');              
                 var humidity = $('<p>').text('Humidity: ' + weatherList[i].main.humidity + ' %');  
-
+                // Appends values directly to HTML, under 'forecast' ID.
                 cityMain.append(date).append(icon).append(temp).append(wind).append(humidity);
                 $("#forecast").append(cityMain);
             }
@@ -118,33 +127,36 @@ function fiveDayForecast(city)
     })
 }
 
-// Get data saved locally
+// Gets data saved locally.
 function getLocalStorage()
 {
+    // 'saved' gets from local storage (the city names).
     var saved = localStorage.getItem("cities");
     var saveArr = [];
-
+    // Join local storage value to new array, split string into substrings.
     saved.trim();
     saveArr.join(saved);
     saveArr = saved.split(',');
+    // Loops saved values into search history.
     for (var i = 0; i < saveArr.length; i++)
     {
         recentSearchList(saveArr[i]);
     }
 }
 
-// Save data locally
+// Saves data locally.
 function setLocalStorage(city)
 {
+    // Gets value from local storage.
     var info = localStorage.getItem("cities");
-
+    // Push local storage value into array, set key + stringify values.
     var saveArr = [];
     saveArr.push(info);
     localStorage.setItem("cities", JSON.stringify(saveArr));
-
+    // Set local storage.
     info = city;
     localStorage.setItem("cities", info);
-
+    // If array is empty, sets item values and appends to search history.
     if (info.indexOf(city) === -1)
     {
         info = info + ',' + city;
@@ -159,3 +171,4 @@ clearButton.on("click", function () {
     document.querySelector("#search-history").innerHTML = "";
     console.log("History cleared.");
   });
+  //----------------------------------End of JS file----------------------------------
